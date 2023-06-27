4e2f9b7fb8018f493c84dfa4c687b59155cc9182
itsik-ba <112100680+itsik-ba@users.noreply.github.com>
2023-06-27 15:22:48 +0300

    i

--- colorbottle/API/players/dist/playersRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var router = express_1["default"].Router();
-var playersCtrl_1 = require("./playersCtrl");
-router
-    .post("/register", playersCtrl_1.addNewPlayer)
-    .post("/login", playersCtrl_1.playerLogin);
-exports["default"] = router;
